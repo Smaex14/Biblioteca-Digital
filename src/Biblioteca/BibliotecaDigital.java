431a9d733eaaff3c07b0197ba00f4b4d68388559
Smaex14 <edwinzuniga164@gmail.com>
2025-09-15 22:27:02 -0600

    Update BibliotecaDigital.java

--- src/Biblioteca/BibliotecaDigital.java
+++ src/Biblioteca/BibliotecaDigital.java
@@ -4,10 +4,9 @@ package Biblioteca;
 public class BiblotecaDigital {
   public static void main(Strings [] args){
            Scanner sc = new Scanner(System.in);
-            int opcion;     
-            int op;
+            int opcion;        
           usuario a=new usuario();
-        do{
+            
         System.out.println("\n--- Menú Principal ---");
         System.out.println("1.Usuario");
         System.out.println("2.Bibliotecario");
@@ -16,8 +15,7 @@ public class BiblotecaDigital {
             
             switch(opcion){
                 case 1:
-                do{
-                System.out.println("====Menu de usuario=====");    
+                System.out.println("====Menu=====");    
                 System.out.println("1.-Iniciar sesion");
                 System.out.println("2.-Registrarse");
                 System.out.println("3.- Dar de baja");
@@ -26,40 +24,115 @@ public class BiblotecaDigital {
                  switch (opcion2) {
                     case 1:
                         a.iniciarSesion();
-                     
+                System.out.println("====Menu de usuario=====");    
+                System.out.println("1.Busqueda de libros por criterio");
+                System.out.println("2.Gestionar prestamos");
+                System.out.println("Seleccione una opcion");
+                int opcion3 = sc.nextInt();
+                
+                switch(opcion3){
+                    case 1:
+                    System.out.println("====Criterios de busqueda=====");    
+                    System.out.println("1.Titulo");
+                    System.out.println("2.Autor");
+                    System.out.println("3.Tema");
+                    System.out.println("Seleccione una opcion");
+                    int opcion4 = sc.nextInt();
+                    switch(opcion4){
+                      case 1:
+                        System.out.println("Ingrse el titulo del libro");
+                        //scan
+                        if(){
+                          System.out.println("=====Resultados=====");
+                        }else{
+                          System.out.println("No se encontraron coincidencias");
+                        }
                         break;
+                      case 2:
+                        System.out.println("Ingrse el autor del libro");
+                        //scan
+                        if(){
+                          System.out.println("=====Resultados=====");
+                        }else{
+                          System.out.println("No se encontraron coincidencias");
+                        }
+                          break;
+                      case 3:
+                        System.out.println("Ingrse el Tema del libro");
+                        //scan
+                        if(){
+                          System.out.println("=====Resultados=====");
+                        }else{
+                          System.out.println("No se encontraron coincidencias");
+                        }
+                          break;
+                    }
+                     break;
+                  case 2:
+                  System.out.println("====Menu de prestamos=====");    
+                  System.out.println("1.Pedir un prestamo");
+                  System.out.println("2.Extender prestamo");
+                  System.out.println("Devolver libro");
+                  System.out.println("Seleccione una opcion");
+                  int opcion5 = sc.nextInt();
+                  switch(opcion5){
+                    case 1:
+                      //pedir prestamo
+                       System.out.println("")
+                          break;
+                    case 2:
+                    //extender fecha del prestamo
+                      break;
+                    case 3:
+                      //devolver libro
+                  }  
+                }
                     case 2:
                      a.registrar();
+                      System.out.println("Ingrese un correo electronico");
+                     //scan
+                      System.out.println("Ingrese un nombre de usuario");
+                     //scan
+                      System.out.println("Ingrese una contrasenia");
+                     //scan
+                     if(){
+                        System.out.println("Usuario registrado correctamente");
+                     }else{
+                        System.out.println("Datos invalidos o el usuario ya se encuentra registrado")
+                     }
                         break;
                    case 3:
                      a.darDebaja();
+                     System.out.println("Ingrese nombre de usuario");
+                    //scan
+                    System.out.println("Ingrese contraseña");
+                     //scan
+                     if(){
+                       System.out.println("El usuario se ha dado de baja exitosamente")
+                     }else{
+                       System.out.println("El usuario no existe o los datos son invalidos");
+                     }
                      break;
-                   case 0:
-                     System.out.println("");
                     default:
                     System.out.println("Opcion no valida");
                         break;
 
-                }}while(op!=0);
+                }
                 case 2:
                   System.out.println("====Menu Bibliotecario====");
                   System.out.println("");
                     break;
-              case 0:
-                System.out.println("Salir");
+
                 default:
                 System.out.println("Opcion no valida");
                     break;
             }
 
-        }while(opcion !=0);
+
                     
                     
             }
 }
-
-
-
 
 
 
