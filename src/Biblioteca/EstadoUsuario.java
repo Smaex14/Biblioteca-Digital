b51007c7856fc386a5c631007409c134ac99ae94
Smaex14 <edwinzuniga164@gmail.com>
2025-09-15 18:40:10 -0600

    Update EstadoUsuario.java

--- src/Biblioteca/EstadoUsuario.java
+++ src/Biblioteca/EstadoUsuario.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
+
 package Biblioteca;
 
-/**
- * Enumeración que representa el estado de un usuario en el sistema.
- */
 public enum EstadoUsuario {
     ACTIVO,
     INACTIVO
 }
+
