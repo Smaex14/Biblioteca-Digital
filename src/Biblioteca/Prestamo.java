79b9aa971ea10aaf35724742fa2b5f083a60322a
Smaex14 <edwinzuniga164@gmail.com>
2025-09-15 18:47:54 -0600

    Update Prestamo.java

--- src/Biblioteca/Prestamo.java
+++ src/Biblioteca/Prestamo.java
@@ -1,8 +1,37 @@
 
 package Biblioteca;
-
+import java.util.*;
 
 public class Prestamo {
+   libro libro;
+   //usuario
+   long idPrestamo;
+   Date fechaPrestamo;
+   Date fechaDevolucion;
+   boolean estadoPr;
+   
+public prestamo(libro libro, long idPrestamo, Date fechaPrestamo, Date fechaDevolucion,
+        boolean estadoPr) {
+    this.libro=libro;
+    this.idPrestamo=idPrestamo;
+    this.fechaPrestamo=fechaPrestamo;
+    this.fechaDevolucion=fechaDevolucion;
+    this.estadoPr=estadoPr;
+}
+
+public void registrarPrestamo(long idPrestamo) {
+    
+    //actualizar disponibilidad
+    //agregar id prestamo con math random
+}
+
+public void registrarDevolucion() {
+    
+}
+
+public void renovarPrestamo() {
+    
+}
 
 }
 
