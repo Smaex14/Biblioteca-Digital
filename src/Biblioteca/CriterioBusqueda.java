e95ef59fdb3a7d3e64c1030eb0e9a7077d733987
Mots095 <ernestopereaxz@gmail.com>
2025-09-15 22:08:56 -0600

    Update CriterioBusqueda.java

--- src/Biblioteca/CriterioBusqueda.java
+++ src/Biblioteca/CriterioBusqueda.java
@@ -1,7 +1,39 @@
-
 package Biblioteca;
 
 public class CriterioBusqueda {
 
-}
+    
+    private String titulo;
+    private String autor;
+    private String tema;
 
+   
+    public CriterioBusqueda() {
+    }
+
+   
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+}
