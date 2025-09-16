ca093e14e80ba19b97d6243fa3efc6fa7fd359cd
Mots095 <ernestopereaxz@gmail.com>
2025-09-15 21:56:18 -0600

    Update Biblioteca.java

--- src/Biblioteca/Biblioteca.java
+++ src/Biblioteca/Biblioteca.java
@@ -1,8 +1,8 @@
-
-package Biblioteca;
-
-
-public class Biblioteca {
-
-}
-
+public List<Libro> buscarLibro(CriterioBusqueda criterio) {
+    List<Libro> resultados = new ArrayList<>();
+for (Libro libro : this.listaDeLibros) {
+        if (libro.getTitulo().equalsIgnoreCase(criterio.getTitulo())) {
+            resultados.add(libro);
+        }
+    }
+    return resultados;
