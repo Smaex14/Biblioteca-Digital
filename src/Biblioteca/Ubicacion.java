2b40a9af2198d89e4165c33870f8bce809c14cac
Smaex14 <edwinzuniga164@gmail.com>
2025-09-15 18:49:05 -0600

    Update Ubicacion.java

--- src/Biblioteca/Ubicacion.java
+++ src/Biblioteca/Ubicacion.java
@@ -1,7 +1,21 @@
 
 package Biblioteca;
 
-public class Ubicacion {
+public class ubicacion {
+    String estante;
+    int fila;
+    String seccion;
+    
+public ubicacion(String estante, int fila, String seccion) {
+    this.estante=estante;
+    this.fila=fila;
+    this.seccion=seccion;
+    }
+public String obtenerUbicacion(){
+    return "Estante: " + estante + "\nFila: " + fila + "\nSección: " + seccion;
 
 }
+    
+}
+
 
