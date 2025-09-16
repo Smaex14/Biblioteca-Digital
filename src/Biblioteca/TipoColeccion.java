ffcb14bf0a0a74808ecd9e8dcc3b5e2b4cf40705
Smaex14 <edwinzuniga164@gmail.com>
2025-09-15 18:41:08 -0600

    Update TipoColeccion.java

--- src/Biblioteca/TipoColeccion.java
+++ src/Biblioteca/TipoColeccion.java
@@ -1,12 +1,10 @@
 
 package Biblioteca;
 
-/**
- * Enumeración que representa el tipo de colección a la que pertenece un libro.
- */
 public enum TipoColeccion {
     FISICO,
     DIGITAL,
     TEMATICO
 }  
+
 
